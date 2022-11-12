a988573948d835e664c82f9031bbc6522d6c37f5
clem <ewsrfewaf>
2022-11-12 20:18:34 +0100

    consolidated api usage, added del/mod to latest

--- src/components/DataTableEntries.vue
+++ src/components/DataTableEntries.vue
@@ -5,9 +5,12 @@ import { decideColor } from "../utils/tableFuncs";
 import { inject, reactive, Ref, ref, watch } from "vue";
 import ReadingCard from "./ReadingCard.vue";
 import BaseModal from "./BaseModal.vue";
+import BaseInfoSlider from "./BaseInfoSlider.vue";
+import South from "../assets/svg/south.svg?component";
 
 import { dataStore } from "../store";
 import TablePaginationClient from "./TablePaginationClient.vue";
+import { addReading } from "../utils/apiFuncs";
 const { t } = useI18n();
 const { d } = useI18n();
 
@@ -21,6 +24,8 @@ const tableData = reactive({
   perPage: 10,
 });
 
+const sort = reactive({ sort_by: "timestamp", order: "desc" });
+
 const firstRow = ref(0);
 
 watch(firstRow, (newVal) => {
@@ -32,20 +37,13 @@ watch(firstRow, (newVal) => {
   }
 });
 
-watch(
-  () => tableData.perPage,
-  (newVal) => {}
-);
-
 const emits = defineEmits<{
   (e: "update", data: Reading, index: number): void;
   (e: "more-data"): void;
+  (e: "request-sort", sort_by: string, order: "asc" | "desc"): void;
 }>();
 
 const token = inject<Ref<string>>("token");
-const apiUrl = inject("apiUrl");
-
-const slice = reactive({ from: 0, to: 20 });
 
 const viewCard = ref(false);
 const cardIndex = ref() as Ref<number>;
@@ -56,49 +54,55 @@ function showCard(item: Reading, index: number) {
   cardIndex.value = index;
   viewCard.value = true;
 }
-async function handleModify() {
-  if (!cardData.value) {
-    console.log("no card data");
-    return;
+
+function handleSort(name: string) {
+  if (name == "date" || name == "time") {
+    name = "timestamp";
   }
-  if (!token) {
-    console.log("not authorized");
-    return;
+  if (name == "day_time") return;
+  if (sort.sort_by == name) {
+    let order: "asc" | "desc" = sort.order == "asc" ? "desc" : "asc";
+    console.log(`Sort by ${name} in ${order} order`);
+    sort.order = order;
+    emits("request-sort", name, order);
+  } else {
+    console.log(`Sort by ${name} in 'desc' order`);
+    sort.sort_by = name;
+    sort.order = "desc";
+    emits("request-sort", name, "desc");
   }
-  const response = await fetch(apiUrl + "reading/modify" + `?id=${cardData.value.id}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token.value,
-    },
-    body: JSON.stringify(cardData.value),
-  });
-  viewCard.value = false;
-  dataStore.updateReading(cardData.value, cardIndex.value);
 }
 
-async function handleDelete() {
-  if (!cardData.value) {
-    console.log("no card data");
-    return;
+function isSortedBy(name: string) {
+  if (sort.sort_by == name) {
+    return true;
+  } else if ((name == "time" || name == "date") && sort.sort_by == "timestamp") {
+    return true;
   }
-  if (!token) {
-    console.log("not authorized");
-    return;
-  }
-  const response = await fetch(apiUrl + "reading/delete" + `?id=${cardData.value.id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token.value,
-    },
-  });
-  dataStore.deleteReading(cardIndex.value);
-  viewCard.value = false;
+  return false;
 }
 
 function reset(data: Reading) {
   cardData.value = { ...data };
+}
+
+//for undo dialogue
+const showUndo = ref(false);
+const lastDeleted: Ref<Reading | undefined> = ref();
+const lastIndex: Ref<number | undefined> = ref();
+
+function showUndoDialogue(item: Reading, index: number) {
+  viewCard.value = false;
+  lastDeleted.value = item;
+  lastIndex.value = index;
+  showUndo.value = true;
+}
+
+function addDeleted() {
+  if (!token || !lastDeleted.value) return;
+  dataStore.addReading(lastDeleted.value, lastIndex.value);
+  addReading(lastDeleted.value, token.value);
+  showUndo.value = false;
 }
 </script>
 <template>
@@ -109,9 +113,16 @@ function reset(data: Reading) {
           <th
             v-if="item.show"
             scope="col"
-            class="border border-gray-400 px-2 py-2"
+            class="relative cursor-default border border-gray-400 px-2 py-2 font-semibold text-gray-600"
+            :class="{ 'text-black': isSortedBy(item.name) }"
+            @click="handleSort(item.name)"
           >
-            {{ $t("header." + item.name) }}
+            <span>{{ $t("header." + item.name) }}</span>
+            <South
+              class="absolute right-0 top-0 scale-50 fill-gray-600 transition-all hover:fill-gray-500"
+              v-if="isSortedBy(item.name)"
+              :class="{ 'rotate-180': sort.order == 'asc' }"
+            ></South>
           </th>
         </template>
       </thead>
@@ -139,16 +150,24 @@ function reset(data: Reading) {
         </template>
       </tbody>
     </table>
+    <BaseInfoSlider
+      @close="showUndo = false"
+      @action="addDeleted"
+      :show="showUndo"
+      :info-text="$t('messages.deleted_entry')"
+      :action-text="$t('messages.undo')"
+    ></BaseInfoSlider>
     <BaseModal
       :showing="viewCard"
       @close="viewCard = false"
     >
       <ReadingCard
         @reset="reset"
-        @modify="handleModify()"
-        @delete-entry="handleDelete()"
+        @modify="viewCard = false"
+        @delete-entry="showUndoDialogue"
         :data="cardData"
         :edit="true"
+        :index="cardIndex"
         v-if="viewCard && cardData"
       ></ReadingCard
     ></BaseModal>
